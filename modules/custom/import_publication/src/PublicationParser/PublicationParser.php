3a14ec590a43b26e95af7b8e9b1bad249f31c298
Mike Sage <mike@sagesolutionsinc.ca>
2016-03-16 17:06:00 -0400

    updated importer regex and added multiple publications to individual node

--- modules/custom/import_publication/src/PublicationParser/PublicationParser.php
+++ modules/custom/import_publication/src/PublicationParser/PublicationParser.php
@@ -7,100 +7,190 @@ namespace Drupal\import_publication\PublicationParser;
  */
 class PublicationParser {
 
-	/*
-		 * Finds the year and returns it
-	*/
-	static public function getYear($publication) {
-		//finds the date wrapped in ()
-		preg_match("/\(.*\d{4}.*\)/", $publication, $matches);
+	protected $publication;
+	protected $remainingData;
+	protected $logger;
 
-		$logger = \Drupal::logger('import_publication');
-		$logger->notice(t("REGEX:<pre>" . print_r($matches, true)) . "</pre>");
+	public function __construct($publication) {
+		$this->publication = $this->convert_smart_quotes($publication);
+		$this->remainingData = $this->publication;
+		$this->logger = \Drupal::logger('import_publication');
+		//$this->logger->notice(t("New Publication String:" . $this->publication));
+
+	}
+
+	/*
+		* Finds the year and returns it
+	*/
+	public function getYear() {
+		//finds the date wrapped in ()
+		preg_match("/\(.*\d{4}.*\)/", $this->publication, $matches);
+
+		//$this->logger->notice(t("REGEX:<pre>" . print_r($matches, true)) . "</pre>");
 
 		if (!empty($matches)) {
 			$wrappedYear = $matches[0];
 			preg_match("/\d{4}/", $wrappedYear, $year);
 
-			$logger->notice(t("REGEX year:<pre>" . print_r($year, true)) . "</pre>");
+			//$this->logger->notice(t("REGEX year:<pre>" . print_r($year, true)) . "</pre>");
+
+			$this->shortenRemainingData($year[0]);
 			return $year[0];
 		}
 
 		//else the date is not wrapped in ().
-		preg_match("/\d{4}/", $publication, $year);
-		$logger->notice(t("REGEX year not wrapped:<pre>" . print_r($year, true)) . "</pre>");
+		preg_match("/\d{4}/", $this->publication, $year);
+		//$this->logger->notice(t("REGEX year not wrapped:<pre>" . print_r($year, true)) . "</pre>");
+
+		$this->shortenRemainingData($year[0]);
 		return $year[0];
 
 	}
 
 	/*
-		 * Returns the title wrapped in <em> tags
+		* Returns the title wrapped in <em> tags
 	*/
-	static public function getTitle($publication) {
-		$openEM = strpos($publication, "<em>") + 4;
-		$closeEM = strpos($publication, "</em>");
-		$title_length = $closeEM - $openEM;
-		return substr($publication, $openEM, $title_length);
+	public function getTitle() {
+		preg_match("/<em>(.*?)<\/em>/", $this->publication, $matches);
+
+		if (!empty($matches)) {
+			$title = $matches[0];
+			$this->shortenRemainingData($title);
+
+			$title = substr($title, 4, -5);
+			return $title;
+
+		}
+
+		$this->logger->warning(t("Could not find Title in " . $this->publication));
+
+		return "";
+
 	}
 
 	/*
-		 * Returns the periodical wrapped in quotes
+		    * Returns the periodical wrapped in quotes
 	*/
-	static public function getPeriodical($publication) {
-		preg_match("/“.*”|\".*\"/", $publication, $matches);
+	public function getPeriodical() {
+		preg_match("/\".*\"/", $this->publication, $matches);
 
 		if (!empty($matches)) {
 			$periodicalData = $matches[0];
+
+			$this->shortenRemainingData($periodicalData);
 			$periodicalData = str_replace("\"", "", $periodicalData);
-			$periodicalData = str_replace("“", "", $periodicalData);
-			$periodicalData = str_replace("”", "", $periodicalData);
 			return $periodicalData;
 		}
-		$logger = \Drupal::logger('import_publication');
-		$logger->warning(t("Could not find periodical in " . $publication));
+		$this->logger->warning(t("Could not find periodical in " . $this->publication));
 
 		return "";
 	}
 
 	/*
-		 * Returns the author(s)
+		* Returns the author(s)
 	*/
-	static public function getAuthors($publication) {
-
-		$logger = \Drupal::logger('import_publication');
+	public function getAuthors() {
 
 		//first start with data using "(with <author name>, yyyy)"
-		preg_match("/\(?with (.*), \d{4}/", $publication, $matches);
+		preg_match("/\(?with (.*), \d{4}\)/", $this->publication, $matches);
 		if (!empty($matches)) {
-			$logger->notice(t("authors:<pre>" . print_r($matches, true)) . "</pre>");
-			return $matches[0];
-		}
-
-		//or it could simply by (with <author names>)
-		preg_match("/\(with (.*)\)/", $publication, $matches);
-		if (!empty($matches)) {
-			$logger->notice(t("authors:<pre>" . print_r($matches, true)) . "</pre>");
-			return $matches[0];
-		}
-
-		//Not found yet, so its probably at the start. remove strong tags
-		$publication = strip_tags($publication, "<em>");
-
-		//not found, so strip the first part of the string, up to a (, ", or <em>, or YYYY characters
-		preg_match("/^([A-Za-z].*?) (\(|\"|“|<em>|\(?\d{4}\)?)/", $publication, $matches);
-		if (!empty($matches)) {
-			$logger->notice(t("authors (at start) :<pre>" . print_r($matches, true)) . "</pre>");
+			//$this->logger->notice(t("authors:<pre>" . print_r($matches, true)) . "</pre>");
 			$authors = $matches[0];
-
-			//check if authors ends with a comma, if so, remove.
-			if (stripos(strrev($authors), ",") === 0) {
-				$authors = substr($authors, 0, -1);
-			}
+			$authors = trim(substr($authors, 5, -7));
+			$this->shortenRemainingData($authors);
 			return $authors;
 		}
 
-		$logger->warning(t("Could not find author in " . $publication));
+		//or it could simply be (with <author names>)
+		preg_match("/\(with (.*)\)/", $this->publication, $matches);
+		if (!empty($matches)) {
+			//$this->logger->notice(t("authors:<pre>" . print_r($matches, true)) . "</pre>");
+			$authors = $matches[0];
+
+			$this->shortenRemainingData($authors);
+
+			$authors = trim(substr($authors, 5, -1));
+
+			return $authors;
+		}
+
+		//Not found yet, so its probably at the start. remove strong tags
+		$publication = strip_tags($this->publication, "<em>");
+		$this->remainingData = strip_tags($this->remainingData, "<em>");
+
+		//not found, so strip the first part of the string, up to a (, ", or <em>, or YYYY characters
+		preg_match("/^([A-Za-z].*?) (\(|\"|<em>|\(?\d{4}\)?)/", $publication, $matches);
+		if (!empty($matches)) {
+			//$this->logger->notice(t("authors (at start) :<pre>" . print_r($matches, true)) . "</pre>");
+			$authors = $matches[0];
+
+			//check if authors ends with a comma or quote, if so, remove.
+			if (stripos(strrev($authors), "\"") === 0) {
+				$authors = trim(substr($authors, 0, -1));
+			}
+
+			if (stripos(strrev($authors), strrev("<em>")) === 0) {
+				$authors = trim(substr($authors, 0, -4));
+			}
+
+			if (stripos(strrev($authors), ",") === 0) {
+				$authors = trim(substr($authors, 0, -1));
+			}
+
+			$this->shortenRemainingData($authors);
+			return $authors;
+		}
+
+		$this->logger->warning(t("Could not find author in " . $publication));
 		return "";
 
+	}
+
+	/*
+		* Location has the rest of the data
+	*/
+	public function getLocation() {
+		$this->remainingData = str_replace("(with , )", "", $this->remainingData);
+		$this->remainingData = str_replace("()", "", $this->remainingData);
+		$this->remainingData = str_replace(",", "", $this->remainingData);
+		$this->remainingData = str_replace(".", "", $this->remainingData);
+
+		return trim(strip_tags($this->remainingData));
+	}
+
+	/*
+		 * takes the RemainingData string, and cuts out the $piece from it.
+	*/
+	private function shortenRemainingData($piece) {
+		$start = strpos($this->remainingData, $piece);
+
+		if ($start === false) {
+			//not found
+		} else {
+			$end = strpos($this->remainingData, $piece) + strlen($piece);
+			$this->remainingData = substr($this->remainingData, 0, $start) . substr($this->remainingData, $end);
+		}
+
+		//$this->logger->notice(t("Remaining String: " . $this->remainingData));
+
+	}
+
+	private function convert_smart_quotes($string) {
+		$search = array(
+			"‘",
+			"’",
+			"“",
+			"”",
+		);
+
+		$replace = array(
+			"'",
+			"'",
+			'"',
+			'"',
+		);
+
+		return str_replace($search, $replace, $string);
 	}
 
 }
