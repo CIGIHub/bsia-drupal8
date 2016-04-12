cf16cf364bc95a725a17114a8e4fa31b2e5e66c9
Natasha Scott <nscott@cigionline.org>
2016-04-12 13:54:00 -0400

    adding mobile support for menu and search

--- themes/custom/bsia/js/bsia_menu.js
+++ themes/custom/bsia/js/bsia_menu.js
@@ -4,63 +4,90 @@ var Menu = Menu || {
     initialize: function () {
       var windowWidth = $(window).width();
       var mobile = true;
+      var mobileBp = 940;
 
       if($('body').hasClass('non-mobile')){
         mobile = false;
       }
 
-      if ($('#main-menu nav').hasClass('open')) {
-          $('#main-menu nav').removeClass('open');
+      if ($('#main-menu').hasClass('open')) {
+          $('#main-menu').removeClass('open');
+      }
+
+      if ($('.search-box').hasClass('open')) {
+          $('.search-box').removeClass('open');
       }
 
       if(mobile === true){
         if(windowWidth >= mobileBp){
           $('.main-nav').hide();
+          $('.search-button').hide();
           $('#main-menu').show();
-          Menu.close();
+          Menu.close($('#main-menu'));
         }
         else{
           $('.main-nav').show();
+          $('.search-button').show();
           $('#main-menu').hide();
-          Menu.close();
+          Menu.close($('#main-menu'));
         }
 
-        $(".main-nav").off('click').on('click', function (e) {
-          //console.log(e);
-          Menu.toggle();
+        var button = null;
+        $(".main-nav").off('click').on('click', function () {
+          button = 'menu';
+          Menu.toggle(button);
         });
+
+        $(".search-button").off('click').on('click', function () {
+          button = 'search';
+          Menu.toggle(button);
+        });
+        
       }
       else{
           $('.main-nav').hide();
+          $('.search-button').hide();
           $('#main-menu').show();
-          Menu.close();
+          Menu.close($('#main-menu'));
       }
     },
-    close: function(){
-        if ($('#main-menu').hasClass('open')) {
-            $('#main-menu').removeClass('open').hide();
+    close: function(selectedItem){
+        if (selectedItem.hasClass('open')) {
+            selectedItem.removeClass('open').hide();
+           
         }
     },
-    open: function() {
-        if (!($('#main-menu').hasClass('open'))) {
-          console.log('open class');
-          $('#main-menu').addClass('open').show();
-        }
+    open: function(selectedItem) {
+        if (!(selectedItem.hasClass('open'))) {
+          selectedItem.addClass('open').show();
+
+         }
 
     },
-    isOpen: function(){
-        return $('#main-menu').hasClass('open');
+    isOpen: function(selectedItem){
+        return selectedItem.hasClass('open');
     },
-    toggle: function () {
-        console.log('toggle');
-        if (Menu.isOpen()) {
-            Menu.close();
-            console.log('closing');
+    toggle: function (button) {
+      var selectedItem = null;
+      if(button === 'menu'){
+        selectedItem = $('#main-menu');
+        if($('.search-box').hasClass('open')){
+          Menu.close($('.search-box'));
         }
-        else {
-            Menu.open();
-            console.log('opening');
+      }
+      if(button === 'search'){
+        selectedItem = $('.search-box');
+        if($('#main-menu').hasClass('open')){
+          Menu.close($('#main-menu'));
         }
+      }
+
+      if (Menu.isOpen(selectedItem)) {
+          Menu.close(selectedItem);
+      }
+      else {
+          Menu.open(selectedItem);
+      }
     },
 };
 
