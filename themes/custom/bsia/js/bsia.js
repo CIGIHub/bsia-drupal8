5ae1f357b0c136c07ac3fb2cde721e19b533ff22
Mike Sage <msage@cigionline.org>
2016-04-21 15:10:40 -0400

    updated scrolling to anchors

--- themes/custom/bsia/js/bsia.js
+++ themes/custom/bsia/js/bsia.js
@@ -108,6 +108,18 @@
          }
      });
 
+    //scroll to anchors
+    $("a[href*='#']:not([href='#'])").click(function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+                }, 1000);
+            return false;
+        }
+    });
+
 
      $(window).resize(function () {
          Menu.initialize();
